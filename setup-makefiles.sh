953a02e00ceb20c961d4b32da72ceb35126f8e51
OodavidsinoO <david.sin@tuta.io>
2022-03-29 12:07:53 +0800

    First bring up LineageOS to Elixir

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -13,9 +13,9 @@ INITIAL_COPYRIGHT_YEAR=2020
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-LINEAGE_ROOT="${MY_DIR}/../../.."
+ASOP_ROOT="${MY_DIR}/../../.."
 
-HELPER="${LINEAGE_ROOT}/vendor/lineage/build/tools/extract_utils.sh"
+HELPER="${ASOP_ROOT}/tools/extract_utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -23,7 +23,7 @@ fi
 source "${HELPER}"
 
 # Initialize the helper
-setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${LINEAGE_ROOT}" true
+setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ASOP_ROOT}" true
 
 # Copyright headers and guards
 write_headers "m1923 m1926"
@@ -36,7 +36,7 @@ write_footers
 if [ -f "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
     # Reinitialize the helper for device
     INITIAL_COPYRIGHT_YEAR="${DEVICE_BRINGUP_YEAR}"
-    setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}" false
+    setup_vendor "${DEVICE}" "${VENDOR}" "${ASOP_ROOT}" false
 
     # Copyright headers and guards
     write_headers
